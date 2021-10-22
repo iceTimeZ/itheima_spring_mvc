66cbe7cfd8c0fb97dc4f1ecf7eee9e0d0f5681e4
周才波 <516345145@qq.com>
2021-10-22 19:45:04 +0800

    first commit

---
+++ src/main/java/com/itheima/GitTest.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+/**
+ * @author Zhou Caibo
+ * @create 2021-10-22-19:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
