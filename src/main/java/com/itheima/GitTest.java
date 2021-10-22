890b3d6c93aef3a8586462819445a6e422f7795d
周才波 <516345145@qq.com>
2021-10-22 19:52:24 +0800

    second commit

--- src/main/java/com/itheima/GitTest.java
+++ src/main/java/com/itheima/GitTest.java
@@ -7,5 +7,6 @@ package com.itheima;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("我是第二个hello,heihei");
     }
 }
